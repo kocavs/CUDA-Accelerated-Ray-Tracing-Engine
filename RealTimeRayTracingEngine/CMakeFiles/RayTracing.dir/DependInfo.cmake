a96982960f24659e5d2f14c08e055ff65375bb6e
Haoran Xie <68669787+kocavs@users.noreply.github.com>
2024-04-01 18:20:10 -0700

    Updating directory structure

---
+++ RealTimeRayTracingEngine/CMakeFiles/RayTracing.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/bill_xie/Desktop/C++/CUDA-Accelerated-Ray-Tracing-Engine/RayTracingInOneWeek/c++/include/hittableList.cpp" "CMakeFiles/RayTracing.dir/include/hittableList.cpp.o" "gcc" "CMakeFiles/RayTracing.dir/include/hittableList.cpp.o.d"
+  "/Users/bill_xie/Desktop/C++/CUDA-Accelerated-Ray-Tracing-Engine/RayTracingInOneWeek/c++/include/renderer.cpp" "CMakeFiles/RayTracing.dir/include/renderer.cpp.o" "gcc" "CMakeFiles/RayTracing.dir/include/renderer.cpp.o.d"
+  "/Users/bill_xie/Desktop/C++/CUDA-Accelerated-Ray-Tracing-Engine/RayTracingInOneWeek/c++/include/sphere.cpp" "CMakeFiles/RayTracing.dir/include/sphere.cpp.o" "gcc" "CMakeFiles/RayTracing.dir/include/sphere.cpp.o.d"
+  "/Users/bill_xie/Desktop/C++/CUDA-Accelerated-Ray-Tracing-Engine/RayTracingInOneWeek/c++/libs/imgui/backends/imgui_impl_glfw.cpp" "CMakeFiles/RayTracing.dir/libs/imgui/backends/imgui_impl_glfw.cpp.o" "gcc" "CMakeFiles/RayTracing.dir/libs/imgui/backends/imgui_impl_glfw.cpp.o.d"
+  "/Users/bill_xie/Desktop/C++/CUDA-Accelerated-Ray-Tracing-Engine/RayTracingInOneWeek/c++/libs/imgui/backends/imgui_impl_opengl3.cpp" "CMakeFiles/RayTracing.dir/libs/imgui/backends/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/RayTracing.dir/libs/imgui/backends/imgui_impl_opengl3.cpp.o.d"
+  "/Users/bill_xie/Desktop/C++/CUDA-Accelerated-Ray-Tracing-Engine/RayTracingInOneWeek/c++/libs/imgui/imgui.cpp" "CMakeFiles/RayTracing.dir/libs/imgui/imgui.cpp.o" "gcc" "CMakeFiles/RayTracing.dir/libs/imgui/imgui.cpp.o.d"
+  "/Users/bill_xie/Desktop/C++/CUDA-Accelerated-Ray-Tracing-Engine/RayTracingInOneWeek/c++/libs/imgui/imgui_draw.cpp" "CMakeFiles/RayTracing.dir/libs/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/RayTracing.dir/libs/imgui/imgui_draw.cpp.o.d"
+  "/Users/bill_xie/Desktop/C++/CUDA-Accelerated-Ray-Tracing-Engine/RayTracingInOneWeek/c++/libs/imgui/imgui_tables.cpp" "CMakeFiles/RayTracing.dir/libs/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/RayTracing.dir/libs/imgui/imgui_tables.cpp.o.d"
+  "/Users/bill_xie/Desktop/C++/CUDA-Accelerated-Ray-Tracing-Engine/RayTracingInOneWeek/c++/libs/imgui/imgui_widgets.cpp" "CMakeFiles/RayTracing.dir/libs/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/RayTracing.dir/libs/imgui/imgui_widgets.cpp.o.d"
+  "/Users/bill_xie/Desktop/C++/CUDA-Accelerated-Ray-Tracing-Engine/RayTracingInOneWeek/c++/src/main.cpp" "CMakeFiles/RayTracing.dir/src/main.cpp.o" "gcc" "CMakeFiles/RayTracing.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ RealTimeRayTracingEngine/CMakeFiles/RayTracing.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RayTracing.dir/include/hittableList.cpp.o"
+  "CMakeFiles/RayTracing.dir/include/hittableList.cpp.o.d"
+  "CMakeFiles/RayTracing.dir/include/renderer.cpp.o"
+  "CMakeFiles/RayTracing.dir/include/renderer.cpp.o.d"
+  "CMakeFiles/RayTracing.dir/include/sphere.cpp.o"
+  "CMakeFiles/RayTracing.dir/include/sphere.cpp.o.d"
+  "CMakeFiles/RayTracing.dir/libs/imgui/backends/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/RayTracing.dir/libs/imgui/backends/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/RayTracing.dir/libs/imgui/backends/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/RayTracing.dir/libs/imgui/backends/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/RayTracing.dir/libs/imgui/imgui.cpp.o"
+  "CMakeFiles/RayTracing.dir/libs/imgui/imgui.cpp.o.d"
+  "CMakeFiles/RayTracing.dir/libs/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/RayTracing.dir/libs/imgui/imgui_draw.cpp.o.d"
+  "CMakeFiles/RayTracing.dir/libs/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/RayTracing.dir/libs/imgui/imgui_tables.cpp.o.d"
+  "CMakeFiles/RayTracing.dir/libs/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/RayTracing.dir/libs/imgui/imgui_widgets.cpp.o.d"
+  "CMakeFiles/RayTracing.dir/src/main.cpp.o"
+  "CMakeFiles/RayTracing.dir/src/main.cpp.o.d"
+  "RayTracing"
+  "RayTracing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RayTracing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
